7794d97464e7656a04b1bcac6119cc472661d775
Deahyun <teraget@gmail.com>
2020-02-20 22:55:31 +0900

    initial commit

---
+++ pages/index_v2.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Layout from '../components/MyLayout'
+
+export default function Index() {
+    return (
+    <Layout>
+        <p>Hello Next.js</p>
+    </Layout>
+        // <div>
+        //     <Header />
+        //     <p>Hello Next.js</p>
+        // </div>
+    )
+}
---
+++ pages/post.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { useRouter } from 'next/router'
+import Layout from '../components/MyLayout'
+
+const Content  = () => {
+  const router = useRouter()
+
+  return (
+    <Layout>
+      <h1>{router.query.title}</h1>
+      <p>This is the blog post content.</p>
+    </Layout>
+  )
+}
+
+const Page = () => (
+  <Layout>
+    <Content />
+  </Layout>
+)
+
+export default Page
